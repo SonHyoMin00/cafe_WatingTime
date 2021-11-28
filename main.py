25f5bb2c7f52fa069965e6fc5fc905d8e7ad2a93
hmson18 <sondo100@hanmail.net>
2021-11-28 21:42:00 +0900

    ì예측값 확인

--- main.py
+++ main.py
@@ -7,15 +7,20 @@ import matplotlib.pyplot as plt
 
 
 def get_xy():
-    cafe = pd.read_csv('data/cafe.csv', index_col=0)
+    # cafe = pd.read_csv('data/cafe.csv', index_col=0)
     # print(cafe)  # [500 rows x 13 columns]
     # print(cafe.values.shape)  # (500, 13)
 
-    values = [cafe['아메리카노 수'], cafe['핫커피 수'], cafe['아이스커피 수'], cafe['핫음료 수'],
-              cafe['아이스음료 수'], cafe['블랜딩음료 수'], cafe['티 수'], cafe['펄 수'], cafe['총 잔 수'],
-              cafe['근무자 수'], cafe['밀린 주문 수'], cafe['대기시간 (+밀린 주문 수)']]
-    values = np.transpose(values)
-    # print(values.shape)  # (500, 12)
+
+    # values = [cafe['아메리카노 수'], cafe['핫커피 수'], cafe['아이스커피 수'], cafe['핫음료 수'],
+    #           cafe['아이스음료 수'], cafe['블랜딩음료 수'], cafe['티 수'], cafe['펄 수'], cafe['총 잔 수'],
+    #           cafe['근무자 수'], cafe['밀린 주문 수'], cafe['대기시간 (+밀린 주문 수)']]
+    cafe = pd.read_csv('data/cafe.csv')
+    cafe = cafe.iloc[:, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13]]
+    print(cafe)
+
+    values = np.transpose(cafe.values)
+    print(values.shape)  # (500, 12)
 
     scaler = preprocessing.MinMaxScaler()  # 최소, 최대 범위를 0~1로
     values = scaler.fit_transform(cafe.values)
@@ -39,30 +44,30 @@ def model_cafe():
 
     model = keras.Sequential()
     model.add(keras.layers.InputLayer(input_shape=x.shape[1:]))
-    model.add(keras.layers.LSTM(512, return_sequences=True))
-    model.add(keras.layers.LSTM(128, return_sequences=True))
-    model.add(keras.layers.LSTM(108, return_sequences=True))
-    model.add(keras.layers.SimpleRNN(56, return_sequences=True))
-    model.add(keras.layers.SimpleRNN(32, return_sequences=False))
-    # hidden stage의 사이즈를 늘리거나 simplernn layer를 늘릴수록 정확도가 높아짐
-    # return_sequences:true->입력한 값만큼,false->1개  # rnn은 x는 3차원 데이터가 들어옴. y는 2차원임
-
-    model.add(keras.layers.Dense(1, activation='sigmoid'))
+    model.add(keras.layers.LSTM(32, return_sequences=True, activation="ReLU"))
+    model.add(keras.layers.LSTM(16, return_sequences=True, activation="ReLU"))
+    model.add(keras.layers.LSTM(8, return_sequences=False, activation="ReLU"))
+    model.add(keras.layers.Dense(1))
     model.summary()
 
-    model.compile(optimizer=keras.optimizers.SGD(0.1),
+    model.compile(optimizer=keras.optimizers.Adam(0.01),
                   loss=keras.losses.mse,
-                  metrics='acc')    # mae는 정답과의 오차
+                  metrics='mse')
 
-    model.fit(x_train, y_train, epochs=1000, verbose=2,
-              validation_split=0.2)
+    model.fit(x_train, y_train, epochs=100, verbose=2, batch_size=32)
+    model.evaluate(x_test, y_test, verbose=2)
     print(model.evaluate(x_test, y_test, verbose=0))
 
-    t = [[[1, 1, 0, 0, 0, 0, 0, 1, 0, 2, 2, 1, 6.1],
-         [2, 0, 0, 0, 2, 1, 2, 0, 0, 5, 2, 1, 11.6],
-         [3, 0, 1, 0, 0, 1, 0, 0, 0, 2, 2, 2, 14.5],
-         [4, 0, 0, 0, 0, 0, 0, 3, 0, 3, 2, 2, 15.1],
-         [5, 2, 0, 1, 0, 0, 0, 0, 0, 3, 2, 3, 19.3]]]
+    t = [[[1, 0, 0, 0, 0, 0, 1, 0, 2, 2, 1, 6.1],
+         [0, 0, 0, 2, 1, 2, 0, 0, 5, 2, 1, 11.6],
+         [0, 1, 0, 0, 1, 0, 0, 0, 2, 2, 2, 14.5],
+         [0, 0, 0, 0, 0, 0, 3, 0, 3, 2, 2, 15.1],
+         [2, 0, 1, 0, 0, 0, 0, 0, 3, 2, 3, 19.3]],
+         [[1, 0, 0, 0, 0, 0, 1, 0, 2, 2, 1, 6.1],
+          [0, 0, 0, 2, 1, 2, 0, 0, 5, 2, 1, 11.6],
+          [0, 1, 0, 0, 1, 0, 0, 0, 2, 2, 2, 14.5],
+          [0, 0, 0, 0, 0, 0, 3, 0, 3, 2, 2, 15.1],
+          [2, 0, 1, 0, 0, 0, 0, 0, 3, 2, 3, 19.3]]]
 
     # line3-7
     p = model.predict(t)
